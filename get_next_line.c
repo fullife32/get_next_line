c3460e532c7a729eb162a73fe345bada18a4e20b
Eithan ASSOULINE <eassouli@e2r8p20.42.fr>
2019-10-30 14:41:45 +0100

    segv

--- get_next_line.c
+++ get_next_line.c
@@ -20,31 +20,29 @@ int	get_next_line(int fd, char **line)
 	ssize_t	i;
 	ssize_t	nread;
 	ssize_t	allread;
+	int		findn;
 	char	next_line[BUFFER_SIZE + 1];
 
 	i = 0;
-	nread = 1;
+	nread = 0;
 	allread = 0;
+	findn = 0;
 	if (BUFFER_SIZE == 0)
-		return (-1);
-	if (!(line = malloc(sizeof(char *) * 2)))
-		return (0);
-	while (nread != 0)
+	while ((nread = read(fd, next_line, BUFFER_SIZE)) != 0)
 	{
-		if ((nread = read(fd, next_line, BUFFER_SIZE)) == -1)
+		if (nread == -1)
 			return (-1);
+		nread += allread;
 		next_line[nread] = '\0';
-		allread += nread;
 	}
-	if (!(line[0] = malloc(sizeof(char) * (allread + 1))))
+	if (!(*line = malloc(sizeof(char) * (allread + 1))))
 		return (0);
 	while (next_line[i] != '\0')
 	{
-		line[0][i] = next_line[i]; 
+		*line[i] = next_line[i];
 		i++;
 	}
-	line[0][i] = '\0';
-	line[1] = 0;
+	*line[i] = '\0';
 	return (0);
 }
 
@@ -52,10 +50,10 @@ int main(int ac, char **av)
 {
 	if (ac > 1)
 	{
-		char **line = NULL;
+		char *line = NULL;
 		int fd = open(av[1], O_RDONLY);
-		get_next_line(fd, line);
-		printf("%s", line[0]);
+		get_next_line(fd, &line);
+		printf("%s", line);
 	}
 	return (0);
 }
