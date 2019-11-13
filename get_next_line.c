080a71e7ede317542c42dae7a521f097ae37d346
Eithan ASSOULINE <eassouli@e3r12p21.42.fr>
2019-11-13 17:25:11 +0100

    norm and add condition

--- get_next_line.c
+++ get_next_line.c
@@ -12,10 +12,8 @@
 
 #include "get_next_line.h"
 
-int	clean(char *buff, char *line)
+int	clean(char *line)
 {
-	if (buff)
-		free(buff);
 	if (line)
 		free(line);
 	return (-1);
@@ -24,44 +22,24 @@ int	clean(char *buff, char *line)
 int	get_next_line(int fd, char **line)
 {
 	int				nread;
-	char			*buff;
 	static t_list	tmp;
 
-	nread = -1;
-	buff = NULL;
+	nread = ft_strlen(tmp.line + isn(tmp.line)) + 1;
 	*line = NULL;
 	if (line == NULL || fd < 0)
 		return (-1);
-	ft_memmove(tmp.line, tmp.line + isn(tmp.line) + 1, ft_strlen(tmp.line + isn(tmp.line)) + 1);
-	if (!(*line = ft_strjoin(*line, tmp.line)))
-		return (clean(buff, *line));
+	ft_memmove(tmp.line, tmp.line + isn(tmp.line) + 1, nread);
+	if (!(*line = ft_strjoin(*line, tmp.line)) && tmp.line[0] != '\0')
+		return (clean(*line));
 	while (isn(tmp.line) == -1)
 	{
 		if ((nread = read(fd, tmp.line, BUFFER_SIZE)) == -1)
-			return (clean(buff, *line));
-		tmp.line[nread] = '\0';
-		if (!(*line = ft_strjoin(*line, tmp.line)))
-			return (clean(buff, *line));
+			return (clean(*line));
 		if (nread == 0)
 			return (0);
+		tmp.line[nread] = '\0';
+		if (!(*line = ft_strjoin(*line, tmp.line)))
+			return (clean(*line));
 	}
 	return (1);
-}
-
-int main(int ac, char **av)
-{
-	(void)ac;
-	int i = 1;
-	char *line = NULL;
-	int fd = (ac == 1) ? 0 : open(av[1], O_RDONLY);
-
-	while (i > 0)
-	{
-		i = get_next_line(fd, &line);
-		printf("%s\n", line);
-		free(line);
-	}
-	printf("%d\n", i);
-	close(fd);
-	return (0);
 }
---
+++ main.c
@@ -0,0 +1,19 @@
+#include "get_next_line.h"
+
+int main(int ac, char **av)
+{
+	(void)ac;
+	int i = 1;
+	char *line = NULL;
+	int fd = (ac == 1) ? 0 : open(av[1], O_RDONLY);
+
+	while (i > 0)
+	{
+		i = get_next_line(fd, &line);
+		printf("%s\n", line);
+		free(line);
+	}
+	printf("%d\n", i);
+	close(fd);
+	return (0);
+}
