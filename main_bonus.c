991b2feb006e9257d446731097d18b2d78fd07da
fullife32 <eithan.assouline6@gmail.com>
2019-11-20 00:28:29 +0100

    add tests

--- main_bonus.c
+++ main_bonus.c
@@ -5,7 +5,7 @@ int main(int ac, char **av)
 	char *line = NULL;
 	int fd1 = 0;
 	int fd2 = 0;
-	//int fd3 = 0;
+	int fd3 = 0;
 	//int fd4 = 0;
 
 	if (ac == 1)
@@ -31,57 +31,71 @@ int main(int ac, char **av)
 	{
 		fd1 = open(av[1], O_RDONLY);
 		fd2 = open(av[2], O_RDONLY);
+		fd3 = open(av[3], O_RDONLY);
 		// fd1
 		get_next_line(fd1, &line);
-		printf("fd 1 : %s\n", line);
+		printf("FD 1 : %s\n", line);
 		free(line);
 		// fd 2
 		get_next_line(fd2, &line);
-		printf("fd 2 : %s\n", line);
+		printf("FD 2 : %s\n", line);
 		free(line);
-		// fd 1
-		get_next_line(fd1, &line);
-		printf("fd 1 : %s\n", line);
-		free(line);
-		// fd 2
-		get_next_line(fd2, &line);
-		printf("fd 2 : %s\n", line);
-		free(line);
-		// fd 1
-		get_next_line(fd1, &line);
-		printf("fd 1 : %s\n", line);
-		free(line);
-		// fd 2
-		get_next_line(fd2, &line);
-		printf("fd 2 : %s\n", line);
+		// fd3
+		get_next_line(fd3, &line);
+		printf("FD 3 : %s\n", line);
 		free(line);
 		// fd1
 		get_next_line(fd1, &line);
-		printf("fd 1 : %s\n", line);
+		printf("FD 1 : %s\n", line);
 		free(line);
 		// fd 2
 		get_next_line(fd2, &line);
-		printf("fd 2 : %s\n", line);
+		printf("FD 2 : %s\n", line);
+		free(line);
+		// fd3
+		get_next_line(fd3, &line);
+		printf("FD 3 : %s\n", line);
 		free(line);
 		// fd1
 		get_next_line(fd1, &line);
-		printf("fd 1 : %s\n", line);
+		printf("FD 1 : %s\n", line);
 		free(line);
 		// fd 2
 		get_next_line(fd2, &line);
-		printf("fd 2 : %s\n", line);
+		printf("FD 2 : %s\n", line);
+		free(line);
+		// fd3
+		get_next_line(fd3, &line);
+		printf("FD 3 : %s\n", line);
 		free(line);
 		// fd1
 		get_next_line(fd1, &line);
-		printf("fd 1 : %s\n", line);
+		printf("FD 1 : %s\n", line);
 		free(line);
 		// fd 2
 		get_next_line(fd2, &line);
-		printf("fd 2 : %s\n", line);
+		printf("FD 2 : %s\n", line);
+		free(line);
+		// fd3
+		get_next_line(fd3, &line);
+		printf("FD 3 : %s\n", line);
+		free(line);
+		// fd1
+		get_next_line(fd1, &line);
+		printf("FD 1 : %s\n", line);
+		free(line);
+		// fd 2
+		get_next_line(fd2, &line);
+		printf("FD 2 : %s\n", line);
+		free(line);
+		// fd3
+		get_next_line(fd3, &line);
+		printf("FD 3 : %s\n", line);
 		free(line);
 
 		close(fd1);
 		close(fd2);
+		close(fd3);
 	}
 	return (0);
 }
