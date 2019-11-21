e0d1349d0e1abe2e8c0f9a2c21b97a4b23ffdd75
fullife32 <eithan.assouline6@gmail.com>
2019-11-22 00:03:01 +0100

    fix main normal

--- main.c
+++ main.c
@@ -5,7 +5,17 @@ int main(int ac, char **av)
 	char *line = NULL;
 	int fd = 0;
 
-	if (ac == 2)
+	if (ac == 1)
+	{
+		while (get_next_line(0, &line) > 0)
+		{
+			printf("%s\n", line);
+			free(line);
+		}
+		printf("%s\n", line);
+		free(line);
+	}
+	else if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
 		while (get_next_line(fd, &line) > 0)
@@ -17,12 +27,5 @@ int main(int ac, char **av)
 		free(line);
 		close(fd);
 	}
-	else if (ac == 1)
-		while (1)
-		{
-			get_next_line(1, &line);
-			printf("%s\n", line);
-			free(line);
-		}
 	return (0);
 }
